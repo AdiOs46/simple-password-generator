14922a045fd7e8ad5c44c24f81a0531856721fb6
Aditya Ojha <adiojha933@gmail.com>
2024-04-18 18:21:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Password Generator in React
+
+- learnt through 'Chai aur React' series.
+- Understood useEffect, useState, useRef in detail.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gen from './components/Gen'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-white text-center text-4xl mt-5'>Password Generator</h1>
+      <Gen />
+    </>
+  )
+}
+
+export default App
